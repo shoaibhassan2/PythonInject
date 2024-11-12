06b3b32594ee8e09827a59066e688e4a48d1226d
shoaibhassan2 <shoaibhassanking059@gmail.com>
2024-11-12 21:41:07 +0500

    Update Injector.c

--- Injector.c
+++ Injector.c
@@ -1,14 +1,22 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <conio.h>
+#include <stdbool.h>
+
+#define RED     "\033[1;31m"  
+#define YELLOW  "\033[1;33m"  
+#define GREEN   "\033[2;32m"  
+#define WHITE   "\x1b[1;37m"  
+#define ORANGE  "\x1b[38;5;208m"  
+#define RESET   "\033[0m"
 DWORD GetProcessID() {
     PROCESSENTRY32 pe32;
     pe32.dwSize = sizeof(PROCESSENTRY32);
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hSnapshot == INVALID_HANDLE_VALUE) {
-        printf("Failed to take process snapshot.\n");
+        printf("%sFailed to take process snapshot.\n%s",RED,RESET);
         return 0;
     }
     const char* python_exes[] = {
@@ -39,7 +47,7 @@ DWORD GetProcessID() {
 int InjectDLL(DWORD processID, const char* dllName) {
     char dllPath[MAX_PATH];
     if (!GetModuleFileName(NULL, dllPath, MAX_PATH)) {
-        printf("Failed to get executable path. Error: %lu\n", GetLastError());
+        printf("%sFailed to get executable path. Error: %lu\n%s",RED,GetLastError(),RESET);
         return -1;
     }
 
@@ -47,25 +55,25 @@ int InjectDLL(DWORD processID, const char* dllName) {
     if (lastSlash != NULL) {
         strcpy(lastSlash + 1, dllName);
     } else {
-        printf("Failed to determine the DLL path.\n");
+        printf("%sFailed to determine the DLL path.\n%s",RED,RESET);
         return -1;
     }
 
     HANDLE hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD, FALSE, processID);
     if (hProcess == NULL) {
-        printf("Failed to open process. Error: %lu\n", GetLastError());
+        printf("%sFailed to open process. Error: %lu\n%s",RED,GetLastError(),RESET);
         return -1;
     }
 
     LPVOID pDllPath = VirtualAllocEx(hProcess, NULL, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
     if (pDllPath == NULL) {
-        printf("Failed to allocate memory in target process. Error: %lu\n", GetLastError());
+        printf("%sFailed to allocate memory in target process. Error: %lu\n%s",RED,GetLastError(),RESET);
         CloseHandle(hProcess);
         return -1;
     }
 
     if (!WriteProcessMemory(hProcess, pDllPath, dllPath, strlen(dllPath) + 1, NULL)) {
-        printf("Failed to write DLL path to target process. Error: %lu\n", GetLastError());
+        printf("%sFailed to write DLL path to target process. Error: %lu\n%s",RED,GetLastError(),RESET);
         VirtualFreeEx(hProcess, pDllPath, 0, MEM_RELEASE);
         CloseHandle(hProcess);
         return -1;
@@ -73,7 +81,7 @@ int InjectDLL(DWORD processID, const char* dllName) {
 
     FARPROC loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
     if (!loadLibraryAddr) {
-        printf("Failed to get address of LoadLibraryA. Error: %lu\n", GetLastError());
+        printf("%sFailed to get address of LoadLibraryA. Error: %lu\n%s",RED,GetLastError(),RESET);
         VirtualFreeEx(hProcess, pDllPath, 0, MEM_RELEASE);
         CloseHandle(hProcess);
         return -1;
@@ -82,7 +90,7 @@ int InjectDLL(DWORD processID, const char* dllName) {
     HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, 
         (LPTHREAD_START_ROUTINE)loadLibraryAddr, pDllPath, 0, NULL);
     if (hThread == NULL) {
-        printf("Failed to create remote thread. Error: %lu\n", GetLastError());
+        printf("%sFailed to create remote thread. Error: %lu\n%s",RED,GetLastError(),RESET);
         VirtualFreeEx(hProcess, pDllPath, 0, MEM_RELEASE);
         CloseHandle(hProcess);
         return -1;
@@ -100,25 +108,23 @@ int InjectDLL(DWORD processID, const char* dllName) {
 }
 
 int main() {
-    printf("Created by shoaib hassan...\n");
+    system("cls");
+    printf("%sCreated by shoaib hassan...%s\n",GREEN,RESET);
     DWORD processID = 0;
 
     while(true){
-        printf("Waiting for python process to start...\n");
+        printf("%sWaiting for python process to start...%s\n",ORANGE,RESET);
         while ((processID = GetProcessID()) == 0) {
             Sleep(1000); 
         }
         const char* dllName = "PyInjector.dll";
         if (InjectDLL(processID, dllName) == 0) {
-            printf("Injection successful!\n");
+            printf("%sInjection successful!\n%s",GREEN,RESET);
         } else {
-            printf("Injection failed.\n");
+            printf("%sInjection failed.\n%s",RED,RESET);
         }
-        printf("Press any key to inject again\n");
+        printf("%sPress any key to inject again\n\n%s",ORANGE,RESET);
         getch();
     }
-    
-
-
     return 0;
 }
